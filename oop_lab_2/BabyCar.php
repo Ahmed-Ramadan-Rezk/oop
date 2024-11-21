72c3b41f3c3e933e626b1548b847777f58b1ff0a
ahmed <aramadan330@yahoo.com>
2024-11-21 10:53:49 +0200

    Adding objects inside of an array and loop through

--- oop_lab_2/BabyCar.php
+++ oop_lab_2/BabyCar.php
@@ -1,21 +1,25 @@
-<?php 
+<?php
 
 
-class  BabyCar extends Product {
+class  BabyCar extends Product
+{
     public $age;
     public $weight;
     private $materials = [];
     private $specialTax = 11;
 
-    public function displayMaterials() {
-        return $this->materials;
+    public function displayMaterials()
+    {
+        return $this->materials[0];
     }
 
-    public function setMaterial($material) {
+    public function setMaterial($material)
+    {
         return $this->materials[] = $material;
     }
 
-    public function getFinalPrice() {
+    public function getFinalPrice()
+    {
         return $this->price + ($this->specialTax / 100) * $this->price;
     }
 }
